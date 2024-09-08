d52060191b608058a9832825092fc090614da2c9
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2024-09-08 22:30:59 +0000

    Initial commit

---
+++ languages/Settings.english.php
@@ -0,0 +1,10 @@
+<?php
+// Version: 2.1.0; Settings
+
+global $settings;
+
+// argument(s): images_url as saved in settings
+$txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
+$txt['theme_description'] = 'The CleanWhite theme from Crip Zone Themes.<br><br>Author: Crip<br><br>Updated to v2.1 by The CripZone:<br>
+							Special thanks to: Sych0, Live627, shawnb61 and Bigguy';
+?>
